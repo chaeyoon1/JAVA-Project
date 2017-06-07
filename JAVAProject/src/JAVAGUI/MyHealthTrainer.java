88492a2e888862f1d8296045303fe253e2b70c37
JUNG <JUNG@192.168.35.170>
2017-06-07 22:27:39 +0900

    6/7 프로젝트 시작

---
+++ JAVAProject/src/JAVAGUI/MyHealthTrainer.java
@@ -0,0 +1,13 @@
+package JAVAGUI;
+
+import javax.swing.*;
+
+public class MyHealthTrainer extends JFrame {
+	public MyHealthTrainer() {
+		getContentPane().setLayout(null);
+	}
+	
+	public static void main(String[] ar){
+		MyHealthTrainer myHealthTrainer = new MyHealthTrainer();
+	}
+}
