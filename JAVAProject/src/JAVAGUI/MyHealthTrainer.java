bad93a240c6d5061a5da486d9d3177f7db041bec
JUNG <JUNG@192.168.35.170>
2017-06-08 00:06:37 +0900

    6/8 날짜, 요일 구현

--- JAVAProject/src/JAVAGUI/MyHealthTrainer.java
+++ JAVAProject/src/JAVAGUI/MyHealthTrainer.java
@@ -1,16 +1,30 @@
 package JAVAGUI;
 
 import javax.swing.*;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
+import java.util.*;
+import java.awt.event.*;
+import java.text.SimpleDateFormat;
+import java.awt.*;
 
 public class MyHealthTrainer extends JFrame {
+	/*요일 
+	*days[calendar.get(Calendar.DAY_OF_WEEK)-1 는 오늘 요일을 나타내는 값
+	*/
+	Calendar calendar = Calendar.getInstance();
+    String []days = {"일","월","화","수","목","금","토"};
+
+	//날짜
+	Date date = new Date();
+	SimpleDateFormat sdformat = new SimpleDateFormat("yyyy년 MM월 dd일"); 
+	String today = sdformat.format(date);
+	
 	/*설정변수
 	 * purposeOption 1:벌크업 2:다이어트 3:힘증가
 	 * locationOption 1:집 2:헬스장
 	 */
 	private int purposeOption = 0;
 	private int locationOption = 0;
+	
 	//메인프레임
 	private JFrame mainFrame;
 	//메인 패널
@@ -28,30 +42,32 @@ public class MyHealthTrainer extends JFrame {
 		mainPanel = new JTabbedPane();
 		mainPanel.setBounds(0, 0, 404, 282);
 		mainFrame.getContentPane().add(mainPanel);
+		
 		routinePanel = new JPanel();
 		dietPanel = new JPanel();
 		motivationPanel = new JPanel();
 		optionPanel = new JPanel();
 		
-		//1.헬스루틴
 		mainPanel.add("헬스루틴",routinePanel);
+		mainPanel.add("식단",dietPanel);
+		mainPanel.add("동기부여",motivationPanel);
+		mainPanel.add("설정",optionPanel);
+		
+		//1.헬스루틴
 		routinePanel.setLayout(null);
 		
 		//2.식단
-		mainPanel.add("식단",dietPanel);
 		dietPanel.setLayout(null);
 		
 		//3.동기부여
-		mainPanel.add("동기부여",motivationPanel);
 		motivationPanel.setLayout(null);
 		
 		//4.설정
-		mainPanel.add("설정",optionPanel);
 		optionPanel.setLayout(null);
 		
 		//4-1 운동목적
 		JLabel lb1 = new JLabel("\uC6B4\uB3D9\uBAA9\uC801");
-		lb1.setBounds(12, 54, 57, 15);
+		lb1.setBounds(12, 109, 57, 15);
 		optionPanel.add(lb1);
 		
 		JRadioButton bt1 = new JRadioButton("\uBC8C\uD06C\uC5C5");
@@ -60,7 +76,7 @@ public class MyHealthTrainer extends JFrame {
 				purposeOption = 1;
 			}
 		});
-		bt1.setBounds(74, 50, 70, 23);
+		bt1.setBounds(74, 105, 70, 23);
 		optionPanel.add(bt1);
 		
 		JRadioButton bt2 = new JRadioButton("\uB2E4\uC774\uC5B4\uD2B8");
@@ -69,7 +85,7 @@ public class MyHealthTrainer extends JFrame {
 				purposeOption = 2;
 			}
 		});
-		bt2.setBounds(148, 50, 79, 23);
+		bt2.setBounds(148, 105, 79, 23);
 		optionPanel.add(bt2);
 		
 		JRadioButton bt3 = new JRadioButton("\uD798\uC99D\uAC00(\uAE30\uB2A5\uC131 \uC6B4\uB3D9)");
@@ -78,7 +94,7 @@ public class MyHealthTrainer extends JFrame {
 				purposeOption = 3;
 			}
 		});
-		bt3.setBounds(231, 50, 140, 23);
+		bt3.setBounds(231, 105, 140, 23);
 		optionPanel.add(bt3);
 		
 		ButtonGroup purposeButton = new ButtonGroup();
@@ -88,7 +104,7 @@ public class MyHealthTrainer extends JFrame {
 		
 		//4-2 운동위치
 		JLabel lb2 = new JLabel("\uC6B4\uB3D9\uC704\uCE58");
-		lb2.setBounds(12, 99, 57, 15);
+		lb2.setBounds(12, 150, 57, 15);
 		optionPanel.add(lb2);
 		
 		JRadioButton bt4 = new JRadioButton("\uC9D1");
@@ -97,7 +113,7 @@ public class MyHealthTrainer extends JFrame {
 				locationOption = 1;
 			}
 		});
-		bt4.setBounds(74, 95, 70, 23);
+		bt4.setBounds(74, 146, 70, 23);
 		optionPanel.add(bt4);
 		
 		JRadioButton bt5 = new JRadioButton("\uD5EC\uC2A4\uC7A5");
@@ -106,12 +122,25 @@ public class MyHealthTrainer extends JFrame {
 				locationOption = 2;
 			}
 		});
-		bt5.setBounds(148, 95, 79, 23);
+		bt5.setBounds(148, 146, 79, 23);
 		optionPanel.add(bt5);
 		
 		ButtonGroup locationButton = new ButtonGroup();
 		locationButton.add(bt4);
 		locationButton.add(bt5);
+		
+		//4-3 설정 시간
+		JLabel datelb = new JLabel("현재 날짜:"+today);
+		datelb.setFont(new Font("굴림", Font.BOLD, 17));
+		datelb.setBounds(0, 0, 227, 30);
+		optionPanel.add(datelb);
+		
+		//4-3 요일
+		JLabel yoillb = new JLabel("오늘은  "+days[calendar.get(Calendar.DAY_OF_WEEK)-1]+"요일");
+		yoillb.setForeground(new Color(139, 69, 19));
+		yoillb.setFont(new Font("굴림", Font.BOLD, 17));
+		yoillb.setBounds(233, 8, 123, 15);
+		optionPanel.add(yoillb);
 		
 		//메인프레임 셋팅
 		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
